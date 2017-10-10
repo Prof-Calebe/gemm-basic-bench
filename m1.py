b4d767429ae7c7019753b04d2785849848ee3350
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2017-10-10 15:05:36 -0300

    Change whitespaces to tabs

--- m1.py
+++ m1.py
@@ -9,11 +9,11 @@ def mult(X,Y,result):
 	count +=1
 	# iterate through rows of X
 	for i in range(len(X)):
-	   # iterate through columns of Y
-	   for j in range(len(Y[0])):
-	       # iterate through rows of Y
-	       for k in range(len(Y)):
-	           result[i][j] += X[i][k] * Y[k][j]
+		# iterate through columns of Y
+		for j in range(len(Y[0])):
+			# iterate through rows of Y
+			for k in range(len(Y)):
+				result[i][j] += X[i][k] * Y[k][j]
 
 
 size=3
