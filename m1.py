56530a59dc6cf5d81f5d8e5e9c25c92498b1ec9c
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2017-10-10 15:03:37 -0300

    First matrix multiplication

---
+++ m1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import numpy as numpy
+from datetime import datetime
+
+count=0
+
+def mult(X,Y,result):
+	global count
+	count +=1
+	# iterate through rows of X
+	for i in range(len(X)):
+	   # iterate through columns of Y
+	   for j in range(len(Y[0])):
+	       # iterate through rows of Y
+	       for k in range(len(Y)):
+	           result[i][j] += X[i][k] * Y[k][j]
+
+
+size=3
+
+X = numpy.random.random((size,size))
+Y = numpy.random.random((size,size))
+result = numpy.zeros(shape=(size,size))
+
+a = datetime.now()
+mult(X,Y,result)
+mult(X,Y,result)
+mult(X,Y,result)
+b = datetime.now()
+print (repr(count)+":"+repr((b-a).total_seconds()/count))
